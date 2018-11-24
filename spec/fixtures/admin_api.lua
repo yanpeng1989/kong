a484e8a2293e9b7f6f9a31f3cbec4baa6596800b
Hisham Muhammad <hisham@gobolinux.org>
2018-11-23 22:25:05 -0200

    tests(admin_api) add an Admin API version of Blueprints
    Introduce an Admin API variant of Blueprints, so that
    entities can be safely created after Kong was already
    started with `start_kong`. This will let us avoid
    needless stopping and restarting, speeding up the tests.

---
+++ spec/fixtures/admin_api.lua
@@ -0,0 +1,65 @@
+local blueprints = require "spec.fixtures.blueprints"
+local helpers = require "spec.helpers"
+local cjson = require "cjson"
+
+
+local function api_send(method, path, body, forced_port)
+  local api_client = helpers.admin_client(nil, forced_port)
+  local res, err = api_client:send({
+    method = method,
+    path = path,
+    headers = {
+      ["Content-Type"] = "application/json"
+    },
+    body = body,
+  })
+  if not res then
+    return nil, err
+  end
+  local resbody = res.status ~= 204 and res:read_body()
+  api_client:close()
+  if res.status == 204 then
+    return nil
+  elseif res.status < 300 then
+    return cjson.decode(resbody)
+  else
+    return nil, "Error " .. tostring(res.status) .. ": " .. resbody
+  end
+end
+
+
+local entities = {
+  "snis",
+  "certificates",
+  "upstreams",
+  "consumers",
+  "targets",
+  "plugins",
+  "routes",
+  "services",
+  "jwt_secrets",
+  "oauth2_credentials",
+  "oauth2_tokens",
+  "oauth2_authorization_codes",
+  "keyauth_credentials",
+  "basicauth_credentials",
+  "hmacauth_credentials",
+}
+
+
+local admin_api_as_db = {}
+
+for _, name in ipairs(entities) do
+  admin_api_as_db[name] = {
+    insert = function(_, tbl)
+      local ok, err = api_send("POST", "/" .. name, tbl)
+      return ok, err
+    end,
+    remove = function(_, tbl)
+      return api_send("DELETE", "/" .. name .. "/" .. tbl.id)
+    end,
+  }
+end
+
+
+return blueprints.new(nil, admin_api_as_db)
